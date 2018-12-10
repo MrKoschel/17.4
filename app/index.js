8c979cc8eaf9674cd66670931b33c836192105b7
MrKoschel <38735374+MrKoschel@users.noreply.github.com>
2018-12-10 15:50:43 +0100

    Add module

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 process.stdin.setEncoding('utf-8');
 var os = require('os');
+var OSinfo = require('../modules/OSinfo');
 process.stdin.on('readable', function () {
     // metoda .read() ma za zadanie odczytac co uzytkownik podal na wejsciu
     var input = process.stdin.read();
@@ -26,21 +27,4 @@ process.stdin.on('readable', function () {
     }
 
 });
-function getOSinfo() {
-    var type = os.type();
-    if(type === 'Darwin') {
-        type = 'OSX';
-    } else if(type === 'Windows_NT') {
-        type = 'Windows';
-    }
-    var release = os.release();
-    var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
-    var userInfo = os.userInfo();
-    console.log('System:', type);
-    console.log('Release:', release);
-    console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
-    console.log('User name:', userInfo.username);
-    console.log('Home dir:', userInfo.homedir);
-}
+
---
+++ modules/time.js
@@ -0,0 +1,17 @@
+process.stdin.setEncoding('utf-8');
+var os = require('os');
+
+    var uptime = os.uptime();
+    function uptimeCalc(d) {
+        d = uptime(d);
+    var h = Math.floor(d / 3600);
+    var m = Math.floor(d % 3600 / 60);
+    var s = Math.floor(d % 3600 % 60);
+
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return hDisplay + mDisplay + sDisplay; 
+    }
+
+exports.print = timeCalc; 
